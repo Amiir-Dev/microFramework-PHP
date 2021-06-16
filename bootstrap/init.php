51ae6a43982cd85b4701a0189250355db65b58a6
Amir Abouei <abouei.me@gmail.com>
2021-06-16 17:21:42 +0430

    Update init.php

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -2,6 +2,8 @@
 
 define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . "/");
 include BASE_PATH . "vendor/autoload.php";
+include BASE_PATH . "helpers/helpers.php";
+
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
@@ -9,10 +11,8 @@ $dotenv->load();
 $request = new App\Core\Request();
 
 
-include BASE_PATH . "helpers/helpers.php";
 
 include BASE_PATH . "routes/web.php";
 
-// include BASE_PATH . "App/Middleware/GlobalMiddleware.php";
 
 
