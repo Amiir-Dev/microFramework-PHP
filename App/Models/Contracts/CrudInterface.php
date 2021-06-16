fece4e313e857342449568d0dd6fff688e0979cc
Amir Abouei <abouei.me@gmail.com>
2021-06-16 17:19:21 +0430

    Update CrudInterface.php

--- App/Models/Contracts/CrudInterface.php
+++ App/Models/Contracts/CrudInterface.php
@@ -2,21 +2,18 @@
 
 namespace App\Models\Contracts;
 
-interface CrudInterface
-{
+interface CrudInterface{
+
     # create (INSERT)
-    public function create(array $data): int;
+    public function create(array $data) : int;
 
+    # Read (SELECT) single | multiple
+    public function find($id) : object;
+    public function get($columns, array $where) : array;
 
-    # Read (SELECT)  Single | Multiple
-    public function find($id): object;
-    public function get(array $columns, array $where): array;
+    # Update records
+    public function update(array $data, array $where) : int;
 
-
-    # Update (UPDATE)
-    public function update(array $data, array $where): int;
-
-
-    # Delete (DELETE)
-    public function delete(array $where): int;
+    # Delete
+    public function delete(array $where) : int;
 }
